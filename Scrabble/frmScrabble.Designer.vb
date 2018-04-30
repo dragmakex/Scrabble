bdb4e18a7f1c02e2be287f10d4306eb746ed403e
Alex <dr4gm4k3x@protonmail.com>
2018-04-30 01:00:32 -0400

    Initial commit

---
+++ Scrabble/frmScrabble.Designer.vb
@@ -0,0 +1,339 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmScrabble
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pic1 = New System.Windows.Forms.PictureBox()
+        Me.pic2 = New System.Windows.Forms.PictureBox()
+        Me.pic3 = New System.Windows.Forms.PictureBox()
+        Me.pic4 = New System.Windows.Forms.PictureBox()
+        Me.pic5 = New System.Windows.Forms.PictureBox()
+        Me.pic6 = New System.Windows.Forms.PictureBox()
+        Me.pic7 = New System.Windows.Forms.PictureBox()
+        Me.cmdCheck = New System.Windows.Forms.Button()
+        Me.lblWord = New System.Windows.Forms.Label()
+        Me.lstDict = New System.Windows.Forms.ListBox()
+        Me.lstTiles = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.GameToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReplaceTilesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.lbl3 = New System.Windows.Forms.Label()
+        Me.lblTime = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.pic1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic7, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pic1
+        '
+        Me.pic1.Location = New System.Drawing.Point(107, 381)
+        Me.pic1.Name = "pic1"
+        Me.pic1.Size = New System.Drawing.Size(150, 150)
+        Me.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic1.TabIndex = 0
+        Me.pic1.TabStop = False
+        '
+        'pic2
+        '
+        Me.pic2.Location = New System.Drawing.Point(305, 381)
+        Me.pic2.Name = "pic2"
+        Me.pic2.Size = New System.Drawing.Size(150, 150)
+        Me.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic2.TabIndex = 1
+        Me.pic2.TabStop = False
+        '
+        'pic3
+        '
+        Me.pic3.Location = New System.Drawing.Point(501, 381)
+        Me.pic3.Name = "pic3"
+        Me.pic3.Size = New System.Drawing.Size(150, 150)
+        Me.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic3.TabIndex = 2
+        Me.pic3.TabStop = False
+        '
+        'pic4
+        '
+        Me.pic4.Location = New System.Drawing.Point(691, 381)
+        Me.pic4.Name = "pic4"
+        Me.pic4.Size = New System.Drawing.Size(150, 150)
+        Me.pic4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic4.TabIndex = 3
+        Me.pic4.TabStop = False
+        '
+        'pic5
+        '
+        Me.pic5.Location = New System.Drawing.Point(886, 381)
+        Me.pic5.Name = "pic5"
+        Me.pic5.Size = New System.Drawing.Size(150, 150)
+        Me.pic5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic5.TabIndex = 4
+        Me.pic5.TabStop = False
+        '
+        'pic6
+        '
+        Me.pic6.Location = New System.Drawing.Point(1080, 381)
+        Me.pic6.Name = "pic6"
+        Me.pic6.Size = New System.Drawing.Size(150, 150)
+        Me.pic6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic6.TabIndex = 5
+        Me.pic6.TabStop = False
+        '
+        'pic7
+        '
+        Me.pic7.Location = New System.Drawing.Point(1274, 381)
+        Me.pic7.Name = "pic7"
+        Me.pic7.Size = New System.Drawing.Size(150, 150)
+        Me.pic7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic7.TabIndex = 6
+        Me.pic7.TabStop = False
+        '
+        'cmdCheck
+        '
+        Me.cmdCheck.BackColor = System.Drawing.Color.DarkBlue
+        Me.cmdCheck.ForeColor = System.Drawing.Color.White
+        Me.cmdCheck.Location = New System.Drawing.Point(572, 290)
+        Me.cmdCheck.Name = "cmdCheck"
+        Me.cmdCheck.Size = New System.Drawing.Size(366, 60)
+        Me.cmdCheck.TabIndex = 7
+        Me.cmdCheck.Text = "Check the Word!"
+        Me.cmdCheck.UseVisualStyleBackColor = False
+        Me.cmdCheck.Visible = False
+        '
+        'lblWord
+        '
+        Me.lblWord.AutoSize = True
+        Me.lblWord.Font = New System.Drawing.Font("Modern No. 20", 36.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWord.Location = New System.Drawing.Point(700, 77)
+        Me.lblWord.Name = "lblWord"
+        Me.lblWord.Size = New System.Drawing.Size(0, 62)
+        Me.lblWord.TabIndex = 8
+        Me.lblWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lstDict
+        '
+        Me.lstDict.FormattingEnabled = True
+        Me.lstDict.ItemHeight = 29
+        Me.lstDict.Location = New System.Drawing.Point(107, 561)
+        Me.lstDict.Name = "lstDict"
+        Me.lstDict.Size = New System.Drawing.Size(295, 178)
+        Me.lstDict.TabIndex = 9
+        '
+        'lstTiles
+        '
+        Me.lstTiles.FormattingEnabled = True
+        Me.lstTiles.ItemHeight = 29
+        Me.lstTiles.Location = New System.Drawing.Point(430, 561)
+        Me.lstTiles.Name = "lstTiles"
+        Me.lstTiles.Size = New System.Drawing.Size(289, 178)
+        Me.lstTiles.TabIndex = 10
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.GameToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1924, 28)
+        Me.MenuStrip1.TabIndex = 11
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'GameToolStripMenuItem
+        '
+        Me.GameToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StartToolStripMenuItem, Me.ClearToolStripMenuItem, Me.ReplaceTilesToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.GameToolStripMenuItem.Name = "GameToolStripMenuItem"
+        Me.GameToolStripMenuItem.Size = New System.Drawing.Size(60, 24)
+        Me.GameToolStripMenuItem.Text = "Game"
+        '
+        'StartToolStripMenuItem
+        '
+        Me.StartToolStripMenuItem.Name = "StartToolStripMenuItem"
+        Me.StartToolStripMenuItem.Size = New System.Drawing.Size(181, 26)
+        Me.StartToolStripMenuItem.Text = "Start"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(181, 26)
+        Me.ClearToolStripMenuItem.Text = "Clear"
+        '
+        'ReplaceTilesToolStripMenuItem
+        '
+        Me.ReplaceTilesToolStripMenuItem.Name = "ReplaceTilesToolStripMenuItem"
+        Me.ReplaceTilesToolStripMenuItem.Size = New System.Drawing.Size(181, 26)
+        Me.ReplaceTilesToolStripMenuItem.Text = "Replace Tiles"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(181, 26)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Modern No. 20", 19.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(880, 561)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(293, 34)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Your Current Score"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblScore
+        '
+        Me.lblScore.AutoSize = True
+        Me.lblScore.Font = New System.Drawing.Font("Modern No. 20", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScore.Location = New System.Drawing.Point(1015, 615)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(31, 34)
+        Me.lblScore.TabIndex = 13
+        Me.lblScore.Text = "0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(764, 580)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 30)
+        Me.Label2.TabIndex = 14
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'lbl3
+        '
+        Me.lbl3.AutoSize = True
+        Me.lbl3.Font = New System.Drawing.Font("Modern No. 20", 19.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl3.Location = New System.Drawing.Point(1188, 561)
+        Me.lbl3.Name = "lbl3"
+        Me.lbl3.Size = New System.Drawing.Size(245, 34)
+        Me.lbl3.TabIndex = 15
+        Me.lbl3.Text = "Time remaining"
+        '
+        'lblTime
+        '
+        Me.lblTime.AutoSize = True
+        Me.lblTime.Location = New System.Drawing.Point(1269, 615)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(55, 30)
+        Me.lblTime.TabIndex = 16
+        Me.lblTime.Text = "120"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(1316, 614)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(24, 30)
+        Me.Label3.TabIndex = 17
+        Me.Label3.Text = "s"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1000
+        '
+        'frmScrabble
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 29.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1924, 1055)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblTime)
+        Me.Controls.Add(Me.lbl3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lstTiles)
+        Me.Controls.Add(Me.lstDict)
+        Me.Controls.Add(Me.lblWord)
+        Me.Controls.Add(Me.cmdCheck)
+        Me.Controls.Add(Me.pic7)
+        Me.Controls.Add(Me.pic6)
+        Me.Controls.Add(Me.pic5)
+        Me.Controls.Add(Me.pic4)
+        Me.Controls.Add(Me.pic3)
+        Me.Controls.Add(Me.pic2)
+        Me.Controls.Add(Me.pic1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Font = New System.Drawing.Font("Modern No. 20", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.Name = "frmScrabble"
+        Me.Text = "frmScrabble"
+        CType(Me.pic1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic7, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pic1 As PictureBox
+    Friend WithEvents pic2 As PictureBox
+    Friend WithEvents pic3 As PictureBox
+    Friend WithEvents pic4 As PictureBox
+    Friend WithEvents pic5 As PictureBox
+    Friend WithEvents pic6 As PictureBox
+    Friend WithEvents pic7 As PictureBox
+    Friend WithEvents cmdCheck As Button
+    Friend WithEvents lblWord As Label
+    Friend WithEvents lstDict As ListBox
+    Friend WithEvents lstTiles As ListBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents GameToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StartToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ReplaceTilesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblScore As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents lbl3 As Label
+    Friend WithEvents lblTime As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ Scrabble/frmScrabble.vb
@@ -0,0 +1,396 @@
+﻿Public Class frmScrabble
+    Dim b As Bag
+    Dim item As String
+    Dim arrpic(7) As PictureBox
+    Dim arrwords(58113) As String
+    Dim dictnumitems As Integer
+    Dim word As String
+    Dim score As Integer
+    Dim tempscore As Integer
+
+    Structure Tile
+        Dim letter As String
+        Dim v As Integer
+
+        Function getTstr() As String
+
+            getTstr = letter + "-" + Str(v)
+
+        End Function
+    End Structure
+
+    Structure Bag
+        Dim arrbag() As Tile
+        Sub Fill()
+            Dim path As String
+
+            ReDim arrbag(100)
+            Dim line As String
+            Dim letter As String
+            Dim value As String
+            Dim num As Integer
+            Dim spacepos1 As Integer
+            Dim spacepos2 As Integer
+            Dim i As Integer
+            Dim c As Integer = 0
+            path = "F:\Locker\Files\Projects\CP II\VB.NET\Scrabble\Scrabble Resources\scrabblevalues.txt"
+            FileOpen(1, path, OpenMode.Input)
+            Do While Not EOF(1)
+                line = Trim(LineInput(1))
+
+                For i = 1 To line.Length
+                    If Mid(line, i, 1) = " " And spacepos1 = Nothing Then
+                        spacepos1 = i
+                    ElseIf Mid(line, i, 1) = " " Then
+                        spacepos2 = i
+                    End If
+                Next
+
+                letter = Mid(line, 1, 1)
+                value = Mid(line, 3, spacepos2 - 3)
+                num = Mid(line, spacepos2 + 1, line.Length - spacepos2)
+
+                For i = 1 To num
+                    arrbag(c).letter = letter
+                    arrbag(c).v = value
+                    c += 1
+                Next
+
+            Loop
+
+            FileClose(1)
+
+
+        End Sub
+
+
+
+        Sub Shuffle()
+            Dim rnum As Integer
+            Dim temp1 As Tile
+
+            Randomize()
+
+            For I = 1 To 100
+                rnum = Int(Rnd() * 100) + 1
+                temp1 = arrbag(I)
+                arrbag(I) = arrbag(rnum)
+                arrbag(rnum) = temp1
+            Next I
+
+
+
+            For i = 1 To 100
+                frmScrabble.lstTiles.Items.Add(arrbag(i).letter & " " & arrbag(i).v)
+            Next
+
+
+        End Sub
+
+        Sub Deal()
+            Dim phand(7) As Tile
+            Dim path As String
+            path = "F:\Locker\Files\Projects\CP II\VB.NET\Scrabble\Tiles\"
+            For i = 1 To 7
+                frmScrabble.arrpic(i).ImageLocation = path + arrbag(i).letter + ".jpg"
+            Next
+        End Sub
+    End Structure
+
+
+    Private Sub frmScrabble_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        setcontrollarray()
+
+    End Sub
+
+    Private Sub lstTiles_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub cmdFill_Click(sender As Object, e As EventArgs)
+
+
+    End Sub
+
+    Private Sub cmdShuff_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub EndToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub FillToolStripMenuItem_Click(sender As Object, e As EventArgs)
+
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        End
+    End Sub
+
+    Sub setcontrollarray()
+        arrpic(1) = pic1
+        arrpic(2) = pic2
+        arrpic(3) = pic3
+        arrpic(4) = pic4
+        arrpic(5) = pic5
+        arrpic(6) = pic6
+        arrpic(7) = pic7
+
+    End Sub
+
+    Sub checkword()
+
+        Dim low As Integer
+        Dim high As Integer
+        Dim mid As Integer
+        Dim pos As Integer
+
+        pos = 0
+        high = 58112
+        low = 1
+
+
+        Do While low <= high
+            mid = Int((low + high) / 2)
+            If Trim(LCase(word)) < Trim(LCase(arrwords(mid))) Then
+                high = mid - 1
+            ElseIf Trim(LCase(word)) > Trim(LCase(arrwords(mid))) Then
+                low = mid + 1
+            ElseIf Trim(LCase(word)) = Trim(LCase(arrwords(mid))) Then
+                pos = mid
+                score = tempscore
+                lblScore.Text = score
+                MsgBox(word)
+                word = ""
+                lblWord.Text = ""
+                MsgBox("Exists!")
+                MsgBox(tempscore)
+
+                Exit Do
+
+            End If
+        Loop
+
+        If pos = 0 Then
+            MsgBox("Doesn't exist!")
+            lblWord.Text = ""
+            word = ""
+            tempscore = 0
+        Else
+            low = 1
+            high = 58112
+            pos = 0
+        End If
+        'binary search
+
+
+    End Sub
+
+    Sub Filldict() 'fill the array of words
+        Dim path As String
+        Dim i As Integer
+
+        dictnumitems = 1
+        path = "F:\Locker\Files\Projects\CP II\VB.NET\Scrabble\Scrabble Resources\dictionary.txt"
+
+        FileOpen(1, path, OpenMode.Input)
+
+        For i = 1 To 58112
+            arrwords(i) = LineInput(1)
+
+            dictnumitems = dictnumitems + 1
+        Next i
+        lstDict.Items.Add(arrwords(1))
+        FileClose(1)
+        Label2.Text = dictnumitems
+    End Sub
+
+    Private Sub StartToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles StartToolStripMenuItem.Click
+
+        lstDict.Items.Clear()
+        lstTiles.Items.Clear()
+        Timer1.Enabled = True
+        For i = 1 To 7
+            arrpic(i).Visible = True
+        Next
+        b.Fill()
+        b.Shuffle()
+        Filldict()
+        b.Deal()
+        cmdCheck.Visible = True
+
+    End Sub
+
+    Private Sub pic1_Click(sender As Object, e As EventArgs) Handles pic1.Click, pic2.Click, pic3.Click, pic4.Click, pic5.Click, pic6.Click, pic7.Click
+        sender.hide() 'clicking on a letter
+        Dim answer As String
+
+        If Mid(sender.imagelocation, 54, 1) = "+" Then
+            answer = Trim(UCase(InputBox("What letter do wish for?")))
+            word = word + answer
+            lblWord.Text = word
+        Else
+            word = word + Mid(sender.imagelocation, 54, 1)
+            lblWord.Text = word
+        End If
+
+        If Mid(sender.imagelocation, 54, 1) = "A" Then
+            tempscore = tempscore + 1
+        ElseIf Mid(sender.imagelocation, 54, 1) = "B" Then
+            tempscore = tempscore + 3
+        ElseIf Mid(sender.imagelocation, 54, 1) = "C" Then
+            tempscore = tempscore + 3
+        ElseIf Mid(sender.imagelocation, 54, 1) = "D" Then
+            tempscore = tempscore + 2
+        ElseIf Mid(sender.imagelocation, 54, 1) = "E" Then
+            tempscore = tempscore + 1
+        ElseIf Mid(sender.imagelocation, 54, 1) = "F" Then
+            tempscore = tempscore + 4
+        ElseIf Mid(sender.imagelocation, 54, 1) = "G" Then
+            tempscore = tempscore + 2
+        ElseIf Mid(sender.imagelocation, 54, 1) = "H" Then
+            tempscore = tempscore + 4
+        ElseIf Mid(sender.imagelocation, 54, 1) = "I" Then
+            tempscore = tempscore + 1
+        ElseIf Mid(sender.imagelocation, 54, 1) = "J" Then
+            tempscore = tempscore + 8
+        ElseIf Mid(sender.imagelocation, 54, 1) = "K" Then
+            tempscore = tempscore + 5
+        ElseIf Mid(sender.imagelocation, 54, 1) = "L" Then
+            tempscore = tempscore + 1
+        ElseIf Mid(sender.imagelocation, 54, 1) = "M" Then
+            tempscore = tempscore + 3
+        ElseIf Mid(sender.imagelocation, 54, 1) = "N" Then
+            tempscore = tempscore + 1
+        ElseIf Mid(sender.imagelocation, 54, 1) = "O" Then
+            tempscore = tempscore + 1
+        ElseIf Mid(sender.imagelocation, 54, 1) = "P" Then
+            tempscore = tempscore + 3
+        ElseIf Mid(sender.imagelocation, 54, 1) = "Q" Then
+            tempscore = tempscore + 10
+        ElseIf Mid(sender.imagelocation, 54, 1) = "R" Then
+            tempscore = tempscore + 1
+        ElseIf Mid(sender.imagelocation, 54, 1) = "S" Then
+            tempscore = tempscore + 1
+        ElseIf Mid(sender.imagelocation, 54, 1) = "T" Then
+            tempscore = tempscore + 1
+        ElseIf Mid(sender.imagelocation, 54, 1) = "U" Then
+            tempscore = tempscore + 1
+        ElseIf Mid(sender.imagelocation, 54, 1) = "V" Then
+            tempscore = tempscore + 4
+        ElseIf Mid(sender.imagelocation, 54, 1) = "W" Then
+            tempscore = tempscore + 4
+        ElseIf Mid(sender.imagelocation, 54, 1) = "X" Then
+            tempscore = tempscore + 8
+        ElseIf Mid(sender.imagelocation, 54, 1) = "Y" Then
+            tempscore = tempscore + 4
+        ElseIf Mid(sender.imagelocation, 54, 1) = "Z" Then
+            tempscore = tempscore + 10
+        ElseIf Mid(sender.imagelocation, 54, 1) = "+" Then
+            tempscore = tempscore + 0
+        End If
+
+        lblScore.Text = score
+    End Sub
+
+    Private Sub ClearToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearToolStripMenuItem.Click
+        lstDict.Items.Clear()
+        lstTiles.Items.Clear()
+        For i = 1 To 7
+            arrpic(i).Visible = False
+        Next
+        lblWord.Text = ""
+        Label2.Text = ""
+        word = ""
+        Timer1.Enabled = False
+        lblTime.Text = "120"
+        lblScore.Text = "0"
+        score = 0
+        tempscore = 0
+    End Sub
+
+    Private Sub cmdCheck_Click(sender As Object, e As EventArgs) Handles cmdCheck.Click
+        checkword()
+    End Sub
+
+    Private Sub ReplaceTilesToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ReplaceTilesToolStripMenuItem.Click
+
+        Dim path As String
+        path = "F:\Locker\Files\Projects\CP II\VB.NET\Scrabble\Tiles\"
+        For i = 1 To 7
+            If arrpic(i).Visible = False Then
+                arrpic(i).ImageLocation = path + b.arrbag(i + 7).letter + ".jpg"
+                arrpic(i).Visible = True
+            End If
+        Next
+    End Sub
+    Sub sortwords()
+        'declare pointer into the array
+        Dim j As Integer
+        'declare the shrink factor as a constant
+        Const shrink = 1.3
+        'declare gap
+        Dim gap As Single
+        'declare space for swapping
+        Dim temp As String
+        Dim swapped As Integer
+        'the indefinite looop stops when list is sorted
+        gap = dictnumitems
+
+        Do
+            gap = Int(gap / shrink)
+            'the gap must not be less than 1
+            If gap < 1 Then gap = 1
+            swapped = False
+            For j = 1 To dictnumitems - gap
+                'compare elements, gap apart
+                If arrwords(j) > arrwords(j + gap) Then
+                    'swap
+                    temp = arrwords(j)
+                    arrwords(j) = arrwords(j + gap)
+                    arrwords(j + gap) = temp
+                    'swap did occur
+                    swapped = True
+                End If
+            Next j
+        Loop Until Not swapped And gap = 1
+
+        'put sorted values into list
+
+    End Sub
+
+    Sub getscore()
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        lblTime.Text = lblTime.Text - 1
+        If lblTime.Text = "0" Then
+            Timer1.Enabled = False
+            Timer2.Enabled = True
+            MsgBox("TIME IS UP!")
+            MsgBox("Your score is:" & " " & score)
+
+            For i = 1 To 7
+                arrpic(i).Enabled = False
+            Next
+            cmdCheck.Enabled = False
+
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
+        If lblTime.Visible = True Then
+                lblTime.Visible = False
+            End If
+
+            If lblTime.Visible = False Then
+                lblTime.Visible = True
+            End If
+
+    End Sub
+End Class
